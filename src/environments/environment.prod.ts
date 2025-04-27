7523e97e654d0b715bf5a2810d4d82d26107da29
Damon Gao <gaochujian159@gmail.com>
2025-04-27 22:48:27 +1000

    Update .gitignore to exclude guide.md and modify angular.json to include file replacements for environment configuration.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  modelServiceUrl: "https://www.tudorai.com.au/model-service/api",
+  securityServiceUrl: "https://www.tudorai.com.au/security-service/api",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  modelServiceUrl: 'http://localhost:4200/model-service/api',
+  securityServiceUrl: 'http://localhost:4200/security-service/api'
+};
