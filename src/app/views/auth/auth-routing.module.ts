ce6c0179d4ac03ca487284e2f0cc3ef90754e5ce
damongaoG <gaochujian159@gmail.com>
2025-05-03 21:19:10 +1000

    Add registration route to authentication module

--- src/app/views/auth/auth-routing.module.ts
+++ src/app/views/auth/auth-routing.module.ts
@@ -1,12 +1,17 @@
 import { LoginComponent } from "@/app/views/auth/login/login.component";
 import { RouterModule, Routes } from "@angular/router";
 import { NgModule } from "@angular/core";
+import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
   {
     path: "login",
     component: LoginComponent,
   },
+  {
+    path:"register",
+    component: RegisterComponent
+  }
 ];
 
 @NgModule({
